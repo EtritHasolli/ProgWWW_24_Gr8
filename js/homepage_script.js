73c88de010cee9d79f36882faa75ff98d61742db
Etrit Hasolli <etrit.hasolli@student.uni-pr.edu>
2024-12-12 15:59:24 +0100

    small bug

--- js/homepage_script.js
+++ js/homepage_script.js
@@ -287,7 +287,7 @@ document.getElementById('openColorPickerBtn').addEventListener('click', function
     // Fetch colors from localStorage
     const headerColor = localStorage.getItem('headerColor') || '#356859';
     const backgroundColor = localStorage.getItem('backgroundColor') || '#f5f5fa';
-    const popColor = localStorage.getItem('popColor') || '#f5f5fa';
+    const popColor = localStorage.getItem('popColor') || '#ffffff';
     const accentColor = localStorage.getItem('accentColor') || '#4a7c68';
     const buttonColor = localStorage.getItem('buttonColor') || '#4a7c68';
 
--- js/study_script.js
+++ js/study_script.js
@@ -60,7 +60,7 @@ document.getElementById('openColorPickerBtn').addEventListener('click', function
     // Fetch colors from localStorage
     const headerColor = localStorage.getItem('headerColor') || '#356859';
     const backgroundColor = localStorage.getItem('backgroundColor') || '#f5f5fa';
-    const pageColor = localStorage.getItem('pageColor') || '#356859';
+    const pageColor = localStorage.getItem('pageColor') || '#e8e1e1';
     const textColor = localStorage.getItem('textColor') || '#4a7c68';
     const buttonColor = localStorage.getItem('buttonColor') || '#4a7c68';
 
